825f246d8d405c8e4b2d100fdaeb24d4b396a957
HamzaAziz-1 <88493437+HamzaAziz-1@users.noreply.github.com>
2022-04-03 20:01:32 +0500

    quiz

---
+++ index.js
@@ -0,0 +1,14 @@
+$(document).ready(function () {
+    $("button").mouseenter(function () {
+        $(".headings , .headings-edu").animate({ left: '300px' });
+    });
+    $("button").mouseleave(function () {
+        $(".headings , .headings-edu").animate({ left: '5px' });
+    });
+});
+
+$(document).ready(function () {
+    $("button").click(function () {
+       
+    });
+});
